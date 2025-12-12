c85536e4479bfe9a1268bd1b867127195b986a9e
parsa <parsafarashah@gmail.com>
2025-12-12 13:08:25 +0330

    style: debug images

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,16 @@ const $optionsState = document.querySelector("#optionsState");
 const $optionsStateLi = document.querySelectorAll("#optionsState>li");
 
 $stateBtn.addEventListener("click", () => {
+  // reset
+  $optionsCity.classList.add("h-0");
+  $optionsCity.classList.remove("h-fit");
+  $cityBtn.children[1].classList.remove("rotate-180");
+
+  $optionsAgency.classList.add("h-0");
+  $optionsAgency.classList.remove("h-fit");
+  $agencyBtn.children[1].classList.remove("rotate-180");
+  // end reset
+
   $optionsState.classList.toggle("h-0");
   $optionsState.classList.toggle("h-fit");
   $stateBtn.children[1].classList.toggle("rotate-180");
@@ -11,9 +21,9 @@ $stateBtn.addEventListener("click", () => {
   $optionsStateLi.forEach((val) => {
     val.addEventListener("click", () => {
       $stateBtn.children[0].children[1].innerText = val.innerText;
-      $optionsState.classList.toggle("h-0");
-      $optionsState.classList.toggle("h-fit");
-      $stateBtn.children[1].classList.toggle("rotate-180");
+      $optionsState.classList.add("h-0");
+      $optionsState.classList.remove("h-fit");
+      $stateBtn.children[1].classList.remove("rotate-180");
     });
   });
 });
@@ -25,6 +35,16 @@ const $optionsCity = document.querySelector("#optionsCity");
 const $optionsCityLi = document.querySelectorAll("#optionsCity>li");
 
 $cityBtn.addEventListener("click", () => {
+  // reset
+  $optionsState.classList.add("h-0");
+  $optionsState.classList.remove("h-fit");
+  $stateBtn.children[1].classList.remove("rotate-180");
+
+  $optionsAgency.classList.add("h-0");
+  $optionsAgency.classList.remove("h-fit");
+  $agencyBtn.children[1].classList.remove("rotate-180");
+  // end reset
+
   $optionsCity.classList.toggle("h-0");
   $optionsCity.classList.toggle("h-fit");
   $cityBtn.children[1].classList.toggle("rotate-180");
@@ -32,9 +52,9 @@ $cityBtn.addEventListener("click", () => {
   $optionsCityLi.forEach((val) => {
     val.addEventListener("click", () => {
       $cityBtn.children[0].children[0].innerText = val.innerText;
-      $optionsCity.classList.toggle("h-0");
-      $optionsCity.classList.toggle("h-fit");
-      $cityBtn.children[1].classList.toggle("rotate-180");
+      $optionsCity.classList.add("h-0");
+      $optionsCity.classList.remove("h-fit");
+      $cityBtn.children[1].classList.remove("rotate-180");
     });
   });
 });
@@ -46,6 +66,16 @@ const $optionsAgency = document.querySelector("#optionsAgency");
 const $optionsAgencyLi = document.querySelectorAll("#optionsAgency>li");
 
 $agencyBtn.addEventListener("click", () => {
+  // reset
+  $optionsCity.classList.add("h-0");
+  $optionsCity.classList.remove("h-fit");
+  $cityBtn.children[1].classList.remove("rotate-180");
+
+  $optionsState.classList.add("h-0");
+  $optionsState.classList.remove("h-fit");
+  $stateBtn.children[1].classList.remove("rotate-180");
+  // end reset
+
   $optionsAgency.classList.toggle("h-0");
   $optionsAgency.classList.toggle("h-fit");
   $agencyBtn.children[1].classList.toggle("rotate-180");
@@ -53,9 +83,9 @@ $agencyBtn.addEventListener("click", () => {
   $optionsAgencyLi.forEach((val) => {
     val.addEventListener("click", () => {
       $agencyBtn.children[0].children[0].innerText = val.innerText;
-      $optionsAgency.classList.toggle("h-0");
-      $optionsAgency.classList.toggle("h-fit");
-      $agencyBtn.children[1].classList.toggle("rotate-180");
+      $optionsAgency.classList.add("h-0");
+      $optionsAgency.classList.remove("h-fit");
+      $agencyBtn.children[1].classList.remove("rotate-180");
     });
   });
 });
